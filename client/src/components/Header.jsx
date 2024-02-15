6d856224f312d600f9e93c57055a9a353f49b6c8
Berkpigh <pierre.berkovits@netplus.ch>
2024-02-15 17:33:11 +0100

    Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { Link } from 'react-router-dom'
 import bankLogo from '../assets/img/argentBankLogo.png'
 
 const Header = () => {
   return (
-    <div>
-      <img src={bankLogo} alt="logo argentBankDB" />
+    <div  className='flex justify-between items-center max-w-8xl mx-auto p-3'>
+      <Link to='/' >
+        <img src={bankLogo} alt="logo argentBankDB" />
+      </Link>
+      <ul className='flex gap-4'>
+          <Link to='/'>
+            <li>Accueil</li>
+          </Link>
+          <Link to='/about'>
+            <li>A propos</li>
+          </Link>
+          <Link to='/sign-in'>
+            <li>Se connecter</li>
+          </Link>
+          <Link to='/sign-in'>
+            <li>S&apos;enregistrer</li>
+          </Link>
+        </ul>
     </div>
   )
 }
